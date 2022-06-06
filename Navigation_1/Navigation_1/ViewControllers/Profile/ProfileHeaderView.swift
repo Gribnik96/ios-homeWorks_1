471ecfbff058639fd7ea7427d0c25b88c95117d3
Nikita Gribin <nikita.gribin@icloud.com>
2022-06-06 18:56:42 +0300

    Done HW4

--- Navigation_1/Navigation_1/ViewControllers/Profile/ProfileHeaderView.swift
+++ Navigation_1/Navigation_1/ViewControllers/Profile/ProfileHeaderView.swift
@@ -9,23 +9,72 @@ import UIKit
 
 class ProfileHeaderView: UIView,UITextFieldDelegate {
     
-    let myImage = UIImageView()
-    let myName = UILabel()
+    //MARK: - Создаём UI-обьекты
+    
+    private var statusText: String = ""
+    
+    lazy var  myImage: UIImageView = {
+        
+        let image = UIImageView()
+        image.frame.size.height = 110
+        image.frame.size.width = 110
+        image.image = UIImage(named: "Dog")
+        image.layer.borderWidth = 3.0
+        image.layer.cornerRadius = image.frame.size.width / 2
+        image.layer.borderColor = UIColor.white.cgColor
+        image.clipsToBounds = true
+        image.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(image)
+        return image
+        
+    }()
+    
+   lazy var myName: UILabel = {
+      
+        let label = UILabel()
+        label.bounds.size = CGSize(width: 100, height: 30)
+        label.text = "Dog"
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(label)
+        return label
+        
+    }()
+    
    lazy var myLabel: UILabel = {
         
-        let label = UILabel(frame: CGRect(x: 34, y: 34, width: 34, height: 23))
+        let label = UILabel()
         label.text = "Waiting for something"
         label.bounds.size = CGSize(width: 200 , height: 30)
         label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
         label.textColor = .gray
         label.translatesAutoresizingMaskIntoConstraints = false
-       addSubview(label)
+        addSubview(label)
         return label
+       
     }()
-    var myTextField = UITextField()
+    
+  lazy  var myTextField: UITextField = {
+        
+        let textField = UITextField()
+        textField.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        textField.textColor = .black
+        textField.layer.cornerRadius = 12
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.placeholder = "Enter text"
+        textField.backgroundColor = .white
+        textField.indent(size: 10)
+        textField.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(textField)
+        return textField
+        
+    }()
+    
     lazy  var myButton: UIButton = {
         
-        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        let button = UIButton()
         button.backgroundColor = .blue
         button.layer.cornerRadius = 14
         button.clipsToBounds = true
@@ -38,97 +87,36 @@ class ProfileHeaderView: UIView,UITextFieldDelegate {
         button.layer.masksToBounds = false
         button.translatesAutoresizingMaskIntoConstraints = false
         button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
-        
         addSubview(button)
-        
         return button
         
     }()
-    private var statusText: String = ""
     
-    
-    
-    
-    
-    func createImage() {
-        
-        myImage.frame.size.height = 130
-        myImage.frame.size.width = 130
-        myImage.image = UIImage(named: "Dog")
-        myImage.layer.borderWidth = 3
-        myImage.layer.borderColor = UIColor.white.cgColor
-        myImage.layer.cornerRadius = myImage.frame.size.width / 2
-        myImage.clipsToBounds = true
-        myImage.translatesAutoresizingMaskIntoConstraints = false
-        
-        addSubview(myImage)
-        
-    }
-    
-    
-    func createNameLabel() {
-        
-        myName.bounds.size = CGSize(width: 100, height: 30)
-        myName.text = "Dog"
-        myName.font = UIFont.boldSystemFont(ofSize: 18)
-        myName.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(myName)
-        
-        
-    }
-    
-    
-    
-    
-    func createTextField() {
-        
-        
-        myTextField.font = UIFont.systemFont(ofSize: 15, weight: .regular)
-        myTextField.textColor = .black
-        myTextField.layer.cornerRadius = 12
-        myTextField.layer.borderWidth = 1
-        myTextField.layer.borderColor = UIColor.black.cgColor
-        myTextField.placeholder = "Enter text"
-        myTextField.backgroundColor = .white
-        myTextField.indent(size: 10)
-        
-        myTextField.delegate = self
-        
-        
-        myTextField.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
-        
-        
-        
-        myTextField.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(myTextField)
-        
-        
-    }
-    
-    
+
+// MARK: - Задаем констрэйнты
     
     
     func setConstraint() {
         
         myImage.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 16).isActive = true
-        myImage.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
-        myImage.widthAnchor.constraint(equalToConstant: 130).isActive = true
-        myImage.heightAnchor.constraint(equalToConstant: 130).isActive = true
+        myImage.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        myImage.widthAnchor.constraint(equalToConstant: 110).isActive = true
+        myImage.heightAnchor.constraint(equalToConstant: 110).isActive = true
         
         myName.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 27).isActive = true
         myName.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 175).isActive = true
         
         
-        myLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 95).isActive = true
+        myLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 75).isActive = true
         myLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 175).isActive = true
         
-        myTextField.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 125).isActive = true
+        myTextField.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 105).isActive = true
         myTextField.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 175).isActive = true
         myTextField.widthAnchor.constraint(equalToConstant: 200).isActive = true
         myTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
         
         
-        myButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 175).isActive = true
+        myButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 155).isActive = true
         myButton.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
         myButton.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
         myButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
@@ -136,21 +124,19 @@ class ProfileHeaderView: UIView,UITextFieldDelegate {
     }
     
     
-    
+    // MARK: - Создаем методы для addTarget (логика)
     
     @objc func statusTextChanged(_ textField: UITextField) {
         
         statusText = textField.text ?? "Error"
         print(statusText)
+        
     }
     
     
     @objc func buttonPressed(_ button: UIButton) {
         
-        print("Click")
-        
-        print(statusText)
-        
+    
         myLabel.text = statusText
         myTextField.endEditing(true)
         myTextField.resignFirstResponder()
@@ -158,6 +144,8 @@ class ProfileHeaderView: UIView,UITextFieldDelegate {
         print(myLabel.text ?? "Nil")
         
     }
+    
+    
     
     
     
